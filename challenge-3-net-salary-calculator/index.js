43cfcee9e738ba89696262d5708baeb6c66c68a2
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-06-16 18:40:42 +0300

    finishing up on paye

--- challenge-3-net-salary-calculator/index.js
+++ challenge-3-net-salary-calculator/index.js
@@ -9,7 +9,7 @@ const personalRelief = 2400
 // TAX PAY
 const taxTier1 = 24000
 const taxTier2 = 32333
-const taxTier3 = 8332
+const taxTier3 = 8333
 
 // PAYEE TAXES
 const payeTier1 = 0.1
@@ -19,6 +19,7 @@ const payeTier3 = 0.3
 // NSSF DEDUCTIONS
 const nssfTier1 = 360
 const nssfTier2 = 720
+const nssfTotal = nssfTier1 + nssfTier2
 
 // NHIF DEDUCTIONS
 
@@ -30,42 +31,67 @@ const nssfTier2 = 720
 
 if(grossPay <= 24000) {
     taxableIncome = (grossPay * payeTier1)
-    console.log(`taxable Income: ${taxableIncome}`)
-    alert((`taxable Income: ${taxableIncome}`))
+    console.log(`Taxable Income: ${taxableIncome}`)
+    alert(`Taxable Income: ${taxableIncome}`)
+    
+    // apply nssf Tier 1 & 2 deductions
+    netPay = grossPay - nssfTotal
+    console.log(`Net salary is: ${netPay}`)
+    alert((`Net salary is: ${netPay}`))
 
     // apply personal relief
     // payeDue = (personalRelief - taxableIncome)
     if(taxableIncome < personalRelief) {
-        alert(`PAYE DUE: 0`)
         console.log((`PAYE DUE: 0`))
+        alert(`PAYE DUE: 0`)
     }
 
 } else if (grossPay > 24000 && grossPay <= 32333) {
+    console.log(`Middle Gross pay: ${grossPay}`)
+
     let tax1 = (taxTier1 * payeTier1) 
-    grossPay = grossPay - tax1
+    grossPay = grossPay - taxTier1
 
     if(grossPay < taxTier3) {
+        // console.log(`less Middle Gross salary is: ${grossPay}`)
+        
         let tax2 = (grossPay * payeTier2)
         taxableIncome = (tax1 + tax2)
-        grossPay = grossPay - taxableIncome
+        console.log(`PAYE DUE before Relief: ${taxableIncome}`)
+        console.log(`PAYE DUE after Relief: ${taxableIncome - personalRelief}`)
+
         console.log(grossPay)
+        grossPay = grossPay - taxableIncome
+        console.log(`Middle Net salary is: ${grossPay}`)
+        alert(`Middle Net slary is: ${grossPay}`)
    
-    } else if (grossPay > taxTier3) {
-        let tax2 = (taxTier3 * payeTier2)
-        taxableIncome = tax1 + tax2
-        grossPay = grossPay - taxableIncome
-        console.log(grossPay)
-    }
+    } 
+    // else if (grossPay > taxTier3) {
+    //     console.log(`Middle 3 Gross salary is: ${grossPay}`)
+    //     let tax2 = (taxTier3 * payeTier2)
+    //     taxableIncome = tax1 + tax2
+    //     grossPay = grossPay - taxableIncome
+    //     console.log(`Middle 3 Net salary is: ${grossPay}`)
+    //     alert(`Middle 3 Net slary is: ${grossPay}`)
+    // }
 
 } else if (grossPay > 32333) {
+    console.log(`end gross salary: ${grossPay}`)
     let tax1 = taxTier1 * payeTier1
-    grossPay = grossPay - tax1
-
     let tax2 = taxTier3 * payeTier2
-    grossPay = grossPay - tax2
+    grossPay = grossPay - (taxTier1 + taxTier3)
 
-    let tax3 = taxTier3 * payeTier3
-    grossPay = grossPay - tax3    
+    let tax3 = grossPay * payeTier3
+
+    taxableIncome = tax1 + tax2 + tax3
+    grossPay = grossPay - taxableIncome 
+    netPay = grossPay
+
+    // console.log(`End Net salary is: ${tax1} ${tax2} ${tax3} ${grossPay}`)
+    console.log(`PAYE DUE before relief: ${taxableIncome}`)
+    console.log(`PAYE DUE after Relief: ${taxableIncome - personalRelief}`)
+    console.log((`End Net slary is: ${netPay}`))
+    alert(`End Net salary is: ${netPay}`) 
 }
 
 // 2. calculates the PAYE Deductions
@@ -75,5 +101,4 @@ if(grossPay <= 24000) {
 
     // PAYEE RELIEFS
     // personal releief  2400
-    // insurance relief  5000
 // 2.1 NHIF | NSSF Deductions
