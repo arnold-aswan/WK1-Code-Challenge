6a785254ab47be74db75810e688b6cc7edf4a47f
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-06-17 16:47:52 +0300

    added NHIF deductions to net calculator

--- net-Salary-Calculator.js
+++ net-Salary-Calculator.js
@@ -51,8 +51,12 @@ if(grossPayTax <= 24000) {
         console.log(`PAYE DUE after personal relief: ${payeDue}`)
     }
     
-    // Calculates & prints the net pay after PAYE deductions
-    netPay = grossPayTax - payeDue
+    // Calculate NHIF 
+    calculateNhif(grossPay)
+    console.log(`NHIF deduction: ${nhif}`)
+
+    // Calculates & prints the net pay after PAYE & NHIF deductions  
+    netPay = grossPayTax - (payeDue + nhif)
     console.log(`Net salary is: ${netPay}`)
     alert((`Net salary is: ${netPay}`))
 
@@ -64,8 +68,7 @@ if(grossPayTax <= 24000) {
     console.log(`Taxble Pay: ${taxableIncome}`)
 
     let tax1 = (taxTier1 * payeTier1) 
-    // grossPay = grossPay - taxTier1
-    // console.log(taxableIncome)
+   
     taxableIncome = taxableIncome - taxTier1
     // console.log(`Taxble pay after tax1: ${taxableIncome}`)
 
@@ -84,9 +87,10 @@ if(grossPayTax <= 24000) {
         payeDue = taxAfterRelief
         console.log(`PAYE DUE after Relief: ${payeDue.toFixed(2)}`)
 
-        // console.log(grossPay)
-        // grossPay = grossPay - taxableIncome
-        netPay = grossPay - payeDue
+        calculateNhif(grossPay)
+        console.log(`NHIF deduction: ${nhif}`)
+
+        netPay = grossPay - (payeDue + nhif)
         // console.log(`Middle Net salary is: ${grossPay}`)
         console.log(`Middle Net salary is: ${netPay}`)
         // alert(`Middle Net slary is: ${grossPay}`)
@@ -102,7 +106,6 @@ if(grossPayTax <= 24000) {
     console.log(`Taxable pay: ${taxableIncome}`)
    
     grossTaxed = grossPayTax - (taxTier1 + taxTier3)
-    console.log(`gross taxed tax3 ${grossTaxed}`)
 
     let tax3 = grossTaxed * payeTier3
     
@@ -115,40 +118,74 @@ if(grossPayTax <= 24000) {
     taxAfterRelief = taxCharged - personalRelief
     payeDue = taxAfterRelief
     console.log(`PAYE DUE after Relief: ${payeDue.toFixed(2)}`)
-    netPay = grossPay - payeDue 
+
+    calculateNhif(grossPay)
+    console.log(`NHIF deduction: ${nhif}`)
+    // console.log(`Total deductions: ${payeDue + nhif +nssfTotal}`)
+    netPay = grossPay - (payeDue + nhif)
 
     console.log((`End Net slary is: ${netPay}`))
     alert(`End Net salary is: ${netPay}`) 
 }
 
-// 2. calculates the PAYE Deductions
-    // <= 24000 10%  
-    // >24000 && <= 32333  25%
-    //  >32333   30%
 
-    // PAYEE RELIEFS
-    // personal releief  2400
-// 2.1 NHIF | NSSF Deductions
+function calculateNhif(grossPay) {
 
-
-
-
-
-// else if (grossPay > taxTier3) {
-            //     console.log(`Middle 3 Gross salary is: ${grossPay}`)
-            //     let tax2 = (taxTier3 * payeTier2)
-            //     taxableIncome = tax1 + tax2
-            //     grossPay = grossPay - taxableIncome
-            //     console.log(`Middle 3 Net salary is: ${grossPay}`)
-            //     alert(`Middle 3 Net slary is: ${grossPay}`)
-            // }
-
-
-// else if (grossPay > taxTier3) {
-    //     console.log(`Middle 3 Gross salary is: ${grossPay}`)
-    //     let tax2 = (taxTier3 * payeTier2)
-    //     taxableIncome = tax1 + tax2
-    //     grossPay = grossPay - taxableIncome
-    //     console.log(`Middle 3 Net salary is: ${grossPay}`)
-    //     alert(`Middle 3 Net slary is: ${grossPay}`)
-    // }
+    switch (true) {
+      case grossPay >= 0 && grossPay <= 5999:
+        nhif = 150;
+        break;
+      case grossPay >= 6000 && grossPay <= 7999:
+        nhif = 300;
+        break;
+      case grossPay >= 8000 && grossPay <= 11999:
+        nhif = 400;
+        break;
+      case grossPay >= 12000 && grossPay <= 14999:
+        nhif = 500;
+        break;
+      case grossPay >= 15000 && grossPay <= 19999:
+        nhif = 600;
+        break;
+      case grossPay >= 20000 && grossPay <= 24999:
+        nhif = 750;
+        break;
+      case grossPay >= 25000 && grossPay <= 29999:
+        nhif = 850;
+        break;
+      case grossPay >= 30000 && grossPay <= 34999:
+        nhif = 900;
+        break;
+      case grossPay >= 35000 && grossPay <= 39999:
+        nhif = 950;
+        break;
+      case grossPay >= 40000 && grossPay <= 44999:
+        nhif = 1000;
+        break;
+      case grossPay >= 45000 && grossPay <= 49999:
+        nhif = 1100;
+        break;
+      case grossPay >= 50000 && grossPay <= 59999:
+        nhif = 1200;
+        break;
+      case grossPay >= 60000 && grossPay <= 69999:
+        nhif = 1300;
+        break;
+      case grossPay >= 70000 && grossPay <= 79999:
+        nhif = 1400;
+        break;
+      case grossPay >= 80000 && grossPay <= 89999:
+        nhif = 1500;
+        break;
+      case grossPay >= 90000 && grossPay <= 99999:
+        nhif = 1600;
+        break;
+      case grossPay >= 100000:
+        nhif = 1700;
+        break;
+      default:
+        nhif = 0;
+    }
+  
+    return nhif;
+  }
